601a49388f0029f780701454226ebd054c75cb1b
Yoshiki Schmitz <yoshikischmitz@outlook.com>
2019-05-26 19:56:26 +0900

    add airbnb inspired example ui

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View, Image } from "react-native";
 import { Box, Text, Card } from "./components";
 import ThemeContext from "./theme-context";
 
@@ -15,6 +15,22 @@ const theme = {
   spaces: [4, 8, 16, 32, 64, 128, 256, 512],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
   breakPoints: [375, 414, 768],
+  fontWeights: ["400", "600", "bold"],
+  borders: [
+    {
+      borderWidth: 1,
+      borderColor: "rgba(0, 0, 0, 0.12)"
+    },
+    {
+      borderWidth: 1,
+      borderColor: "rgba(0, 0, 0, 0.24)"
+    },
+    {
+      borderWidth: 1,
+      borderColor: "rgba(0, 0, 0, 0.36)"
+    }
+  ],
+  borderRadii: [4, 8, 16, 32, 999999],
   shadows: [
     {
       elevation: 1,
@@ -48,73 +64,113 @@ const theme = {
     }
   ],
   colors: {
-    orange: "blue"
+    orange: "blue",
+    green: "#46BB7C",
+    gray0: "rgba(0, 0, 0, 0.5)"
   }
 };
+
+function AirCard({ name }) {
+  return (
+    <Card
+      shadow={1}
+      borderRadius={0}
+      border={2}
+      style={{
+        backgroundColor: "white"
+      }}
+      mr={2}
+    >
+      <Image
+        style={{ width: 8 * 18, height: 80 }}
+        source={{
+          uri:
+            "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
+        }}
+      />
+      <Box pa={1} mb={2}>
+        <Text fontWeight={1} fontSize={2}>
+          {name}
+        </Text>
+      </Box>
+    </Card>
+  );
+}
+
+const HomeCard = ({ name, label, price, rating }) => (
+  <Card mt={1} mh={1} border={0} style={{ flex: 1, minWidth: "50%" }}>
+    <Text color="red" fontSize={1}>
+      {label}
+    </Text>
+    <Box mv={1}>
+      <Text fontSize={2}>{name}</Text>
+    </Box>
+    <Text fontSize={2}>{price}</Text>
+    <Text fontSize={0}>{rating}</Text>
+  </Card>
+);
 
 export default class App extends Component<Props> {
   render() {
     return (
       <ThemeContext.Provider value={theme}>
-        <Box mh={3} mt={5}>
-          <Card
-            shadow={0}
-            mh={2}
-            pa={3}
-            style={{
-              borderWidth: 1,
-              backgroundColor: "white",
-              borderColor: "grey",
-              borderRadius: 3
-            }}
-          >
-            <Text fontSize={4} color="orange">
-              What's up
+        <Box mh={2} mt={3}>
+          <Card pa={2} shadow={1} style={{ backgroundColor: "white" }}>
+            <Text fontSize={1} color="gray0">
+              Try "Paris"
             </Text>
           </Card>
-          <Card
-            shadow={1}
-            mt={3}
-            mh={2}
-            pa={3}
-            style={{
-              borderWidth: 1,
-              backgroundColor: "white",
-              borderColor: "grey",
-              borderRadius: 3
-            }}
-          >
-            <Text fontSize={4} color="orange">
-              What's up
+          <Box mt={2} style={{ flexDirection: "row" }}>
+            <Card mr={1} pa={1} border={0} borderRadius={0}>
+              <Text>Dates</Text>
+            </Card>
+            <Card pa={1} border={0} borderRadius={0}>
+              <Text>Guests</Text>
+            </Card>
+          </Box>
+          <Box mv={3}>
+            <Text fontSize={4} fontWeight={2}>
+              What can we help you find, Yoshiki?
             </Text>
-          </Card>
-          <Card
-            shadow={2}
-            mt={3}
-            mh={2}
-            pa={3}
-            style={{
-              borderWidth: 1,
-              backgroundColor: "white",
-              borderColor: "grey",
-              borderRadius: 3
-            }}
-          >
-            <Text fontSize={4} color="orange">
-              What's up
+          </Box>
+          <Box style={{ flexDirection: "row" }}>
+            <AirCard name="homes" />
+            <AirCard name="Experiences" />
+            <AirCard name="Restaurants" />
+          </Box>
+          <Box mt={3}>
+            <Text fontSize={4} fontWeight={2}>
+              Homes around the world
             </Text>
-          </Card>
+            <Box
+              style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                marginHorizontal: -8
+              }}
+            >
+              <HomeCard
+                label="CAVE CHANIA"
+                name="Luxurious stone villa in Crete"
+                price="$99 per night"
+                rating={<Text color="green">xxxxx - 551 superhost</Text>}
+              />
+              <HomeCard
+                label="CAVE CHANIA"
+                name="Luxurious stone villa in Crete"
+                price="$99 per night"
+                rating={<Text color="green">xxxxx - 551 superhost</Text>}
+              />
+              <HomeCard
+                label="CAVE CHANIA"
+                name="Luxurious stone villa in Crete"
+                price="$99 per night"
+                rating={<Text color="green">xxxxx - 551 superhost</Text>}
+              />
+            </Box>
+          </Box>
         </Box>
       </ThemeContext.Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  }
-});
