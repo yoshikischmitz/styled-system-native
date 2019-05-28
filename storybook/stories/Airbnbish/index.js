4c225a3183dcfc3e5ef452f5f7660aed5e1bb4d5
Yoshiki Schmitz <yoshikischmitz@outlook.com>
2019-05-28 20:02:56 +0900

    refactor example

--- storybook/stories/Airbnbish/index.js
+++ storybook/stories/Airbnbish/index.js
@@ -4,6 +4,36 @@ import { Box, Flex, Text, Span, Card } from "components";
 import theme from "./theme";
 import ThemeContext from "styled-system-native/theme-context";
 
+const data = {
+  cards: [
+    {
+      title: "Homes",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
+    },
+    {
+      title: "Experiences",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
+    },
+    {
+      title: "Resorts",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
+    }
+  ],
+  homes: [
+    {
+      id: 0,
+      label: "CAVE CHANIA",
+      title: "Luxurious stone villa in Crete",
+      price: "$99 per night",
+      rating: 5,
+      ratingMessage: "superhost"
+    }
+  ]
+};
+
 function AirCard({ title, image, ...rest }) {
   return (
     <Card
@@ -41,56 +71,30 @@ const HomeCard = ({ title, label, price, rating, ...rest }) => (
   </Card>
 );
 
-const data = {
-  cards: [
-    {
-      title: "Homes",
-      image:
-        "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
-    },
-    {
-      title: "Experiences",
-      image:
-        "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
-    },
-    {
-      title: "Resorts",
-      image:
-        "https://cdn.vox-cdn.com/thumbor/CTluvlc9kScZlylzsRR4QRCE4Gg=/6x0:641x423/1200x800/filters:focal(6x0:641x423)/cdn.vox-cdn.com/uploads/chorus_image/image/48767301/Screen_Shot_2016-02-09_at_9.08.28_AM.0.0.png"
-    }
-  ],
-  homes: [
-    {
-      id: 0,
-      label: "CAVE CHANIA",
-      title: "Luxurious stone villa in Crete",
-      price: "$99 per night",
-      rating: 5,
-      ratingMessage: "superhost"
-    }
-  ]
-};
+const Search = props => (
+  <Card shadow={1} backgroundColor="backgroundColor" {...props}>
+    <Text fontSize={1} color="gray0">
+      Try <Span color="red">"Paris"</Span>
+    </Text>
+  </Card>
+);
+
+const Filter = ({ children }) => (
+  <Card mr={1} pa={1} border={0} borderRadius={0}>
+    <Text>{children}</Text>
+  </Card>
+);
 
 export default class App extends Component<Props> {
   render() {
     return (
       <ThemeContext.Provider value={theme}>
         <Box mh={2} mt={3}>
-          <Card pa={2} shadow={1} backgroundColor="backgroundColor">
-            <Text fontSize={1} color="gray0">
-              Try <Span color="red">"Paris"</Span>
-            </Text>
-          </Card>
-          <Box mt={2}>
-            <Flex flexDirection="row">
-              <Card mr={1} pa={1} border={0} borderRadius={0}>
-                <Text>Dates</Text>
-              </Card>
-              <Card pa={1} border={0} borderRadius={0}>
-                <Text>Guests</Text>
-              </Card>
-            </Flex>
-          </Box>
+          <Search pa={2} />
+          <Flex mt={2} flexDirection="row">
+            <Filter>Dates</Filter>
+            <Filter>Guests</Filter>
+          </Flex>
           <Box mv={3}>
             <Text fontSize={4} fontWeight={2}>
               What can we help you find, Yoshiki?
@@ -109,9 +113,9 @@ export default class App extends Component<Props> {
               {data.homes.map(
                 ({ id, label, title, price, rating, ratingMessage }) => (
                   <HomeCard
+                    key={id}
                     mt={1}
                     mh={1}
-                    key={id}
                     label="CAVE CHANIA"
                     title="Luxurious stone villa in Crete"
                     price="$99 per night"
